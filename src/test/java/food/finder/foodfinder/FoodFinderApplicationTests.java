027e75c04e123354099ad611798c04dea99d6cec
Kyle R. Miller <krm8mc@mst.edu>
2022-02-21 22:35:02 -0600

    First commit

---
+++ src/test/java/food/finder/foodfinder/FoodFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package food.finder.foodfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
