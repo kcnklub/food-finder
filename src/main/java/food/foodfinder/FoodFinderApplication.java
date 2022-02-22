ed46b5c0531ad56ae18510e507ce5ecbf24f3a25
Kyle R. Miller <krm8mc@mst.edu>
2022-02-21 22:45:13 -0600

    Setting up neo4j database.

--- src/main/java/food/foodfinder/FoodFinderApplication.java
+++ src/main/java/food/foodfinder/FoodFinderApplication.java
@@ -1,13 +1,11 @@
-package food.finder.foodfinder;
+package food.foodfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FoodFinderApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FoodFinderApplication.class, args);
 	}
-
 }
