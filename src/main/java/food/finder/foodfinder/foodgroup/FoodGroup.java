f63ab4b558066739bb661cfe3a42d924439989fc
Andrew Speelman <as7xf@mst.edu>
2022-04-12 20:27:45 -0500

    Fixed wildcard import

--- src/main/java/food/finder/foodfinder/foodgroup/FoodGroup.java
+++ src/main/java/food/finder/foodfinder/foodgroup/FoodGroup.java
@@ -4,7 +4,11 @@ import food.finder.foodfinder.user.User;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.neo4j.core.schema.*;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Property;
+import org.springframework.data.neo4j.core.schema.Relationship;
 
 import java.util.Date;
 import java.util.HashSet;
